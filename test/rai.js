99da70bf881595c280ada2565168f6a3dd7c12b7
Andris Reinman <andris@node.ee>
2012-01-18 15:06:42 +0200

    added possibility to create tests

---
+++ test/rai.js
@@ -0,0 +1,10 @@
+var MailParser = require("../lib/rai").RAIServer,
+    testCase = require('nodeunit').testCase,
+    utillib = require("util");
+
+
+exports["General tests"] = {
+    "Test1": function(test){
+        test.done();
+    }
+}
