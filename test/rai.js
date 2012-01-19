1d143ba27d8ea88969239e12bcceaa8fd752a0a9
Andris Reinman <andris@node.ee>
2012-01-19 11:31:46 +0200

    added some tests, fixed data mode parsing

--- test/rai.js
+++ test/rai.js
@@ -25,7 +25,7 @@ exports["General tests"] = {
             
             var duplicate = new RAIServer();
             duplicate.listen(PORT_NUMBER, function(err){
-                test.ok(err, "Responds with error")
+                test.ok(err, "Responds with error");
                 server.end(function(){
                     test.ok(1, "Server closed");
                     test.done();
@@ -49,34 +49,15 @@ exports["General tests"] = {
                         test.done();
                     });
                 });
-            });
-            
-            var client = netlib.connect(PORT_NUMBER, function(){
-                test.ok(1, "Connected to server");
-                client.end();
-            });
-
-        });
-    },
-    "Timeout": function(test){
-        var server = new RAIServer({timeout: 300});
-        test.expect(3);
-        server.listen(PORT_NUMBER, function(err){
-            
-            server.on("connection", function(socket){
-                test.ok(socket, "Client connected");
                 
-                socket.on("timeout", function(){
-                    test.ok(1, "Connection closed");
-                    
-                    server.end(function(){
-                        test.done();
-                    });
+                socket.on("error", function(err){
+                    test.isError(err);
                 });
             });
             
             var client = netlib.connect(PORT_NUMBER, function(){
                 test.ok(1, "Connected to server");
+                client.end();
             });
 
         });
@@ -96,6 +77,11 @@ exports["General tests"] = {
                         test.done();
                     });
                 });
+                
+                socket.on("error", function(err){
+                    test.isError(err);
+                });
+                
                 socket.end();
             });
             
@@ -104,10 +90,133 @@ exports["General tests"] = {
             });
             client.on("end", function(){
                 test.ok(1, "Connection closed by host");
-            })
+            });
 
         });
     },
+    "Send data to client":  function(test){
+        var server = new RAIServer();
+        server.listen(PORT_NUMBER, function(err){
+            
+            server.on("connection", function(socket){
+                
+                socket.send("HELLO");
+
+                socket.on("end", function(){
+                    server.end(function(){
+                        test.done();
+                    });
+                });
+                
+                socket.on("error", function(err){
+                    test.isError(err);
+                });
+            });
+            
+            var client = netlib.connect(PORT_NUMBER, function(){
+                client.on("data", function(chunk){
+                    test.equal(chunk.toString(), "HELLO\r\n");
+                    client.end();
+                });
+            });
+
+        });
+    }
+};
+
+exports["Secure connection"] = {
+    "STARTTLS with event":  function(test){
+        var server = new RAIServer();
+        server.listen(PORT_NUMBER, function(err){
+            
+            test.expect(2);
+            
+            server.on("connection", function(socket){
+                
+                socket.startTLS();
+                socket.on("tls", function(){
+                    test.ok(1, "Secure connection opened");
+                    socket.send("TEST");
+                });
+                
+                socket.on("end", function(){
+                    server.end(function(){
+                        test.done();
+                    });
+                });
+                
+                socket.on("error", function(err){
+                    test.isError(err);
+                });
+            });
+            
+            var client = netlib.connect(PORT_NUMBER, function(){
+                var sslcontext = crypto.createCredentials();
+                var pair = tlslib.createSecurePair(sslcontext, false);
+                
+                pair.encrypted.pipe(client);
+                client.pipe(pair.encrypted);
+                pair.fd = client.fd;
+                
+                pair.on("secure", function(){
+                    pair.cleartext.on("data", function(chunk){
+                        test.equal(chunk.toString(), "TEST\r\n");
+                        pair.cleartext.end();
+                    });
+                });
+            });
+
+        });
+    },
+    "STARTTLS Callback":  function(test){
+        var server = new RAIServer();
+        server.listen(PORT_NUMBER, function(err){
+            
+            test.expect(2);
+            
+            server.on("connection", function(socket){
+                
+                socket.startTLS(function(){
+                    test.ok(1, "Secure connection opened");
+                    socket.send("TEST");
+                });
+                
+                socket.on("tls", function(){
+                    test.ok(0, "Should not occur");
+                });
+                
+                socket.on("end", function(){
+                    server.end(function(){
+                        test.done();
+                    });
+                });
+                
+                socket.on("error", function(err){
+                    test.isError(err);
+                });
+            });
+            
+            var client = netlib.connect(PORT_NUMBER, function(){
+                var sslcontext = crypto.createCredentials();
+                var pair = tlslib.createSecurePair(sslcontext, false);
+                
+                pair.encrypted.pipe(client);
+                client.pipe(pair.encrypted);
+                pair.fd = client.fd;
+                
+                pair.on("secure", function(){
+                    pair.cleartext.on("data", function(chunk){
+                        test.equal(chunk.toString(), "TEST\r\n");
+                        pair.cleartext.end();
+                    });
+                });
+            });
+
+        });
+    }
+};
+
+exports["Client commands"] = {
     "Receive Simple Command":  function(test){
         var server = new RAIServer();
         server.listen(PORT_NUMBER, function(err){
@@ -121,6 +230,10 @@ exports["General tests"] = {
                     server.end(function(){
                         test.done();
                     });
+                });
+                
+                socket.on("error", function(err){
+                    test.isError(err);
                 });
             });
             
@@ -145,6 +258,10 @@ exports["General tests"] = {
                         test.done();
                     });
                 });
+                
+                socket.on("error", function(err){
+                    test.isError(err);
+                });
             });
             
             var client = netlib.connect(PORT_NUMBER, function(){
@@ -152,31 +269,10 @@ exports["General tests"] = {
             });
 
         });
-    },
-    "Send data to client":  function(test){
-        var server = new RAIServer();
-        server.listen(PORT_NUMBER, function(err){
-            
-            server.on("connection", function(socket){
-                
-                socket.send("HELLO");
+    }
+};
 
-                socket.on("end", function(){
-                    server.end(function(){
-                        test.done();
-                    });
-                });
-            });
-            
-            var client = netlib.connect(PORT_NUMBER, function(){
-                client.on("data", function(chunk){
-                    test.equal(chunk.toString(), "HELLO\r\n");
-                    client.end();
-                });
-            });
-
-        });
-    },
+exports["Data mode"] = {
     "DATA mode": function(test){
         var server = new RAIServer(),
             datapayload = "tere\r\nvana kere";
@@ -199,6 +295,9 @@ exports["General tests"] = {
                     });
                 });
                 
+                socket.on("error", function(err){
+                    test.isError(err);
+                });
             });
             
             var client = netlib.connect(PORT_NUMBER, function(){
@@ -208,11 +307,132 @@ exports["General tests"] = {
 
         });
     },
-    "STARTTLS":  function(test){
+    "Small chunks DATA mode": function(test){
+        var server = new RAIServer(),
+            datapayload = "tere\r\nvana kere õäöü\r\n.\r",
+            databytes = [],
+            fullpayload = datapayload+"\r\n.\r\n";
+        server.listen(PORT_NUMBER, function(err){
+            
+            server.on("connection", function(socket){
+                
+                socket.startDataMode();
+
+                test.expect(1);
+
+                socket.on("data", function(chunk){
+                    databytes = databytes.concat(Array.prototype.slice.call(chunk));
+                });
+                
+                socket.on("ready", function(){
+                    test.equal(new Buffer(databytes).toString("utf-8"), datapayload);
+                    server.end(function(){
+                        test.done();
+                    });
+                });
+                
+                socket.on("error", function(err){
+                    test.isError(err);
+                });
+                
+                for(var i=0, len = fullpayload.length; i<len; i++){
+                    socket._onReceiveData(new Buffer(fullpayload.charAt(i), "utf-8").toString("binary"));
+                }
+                
+            });
+            
+            var client = netlib.connect(PORT_NUMBER, function(){
+                client.end();
+            });
+
+        });
+    }
+};
+
+exports["Pipelining support"] = {
+    "Pipelining": function(test){
         var server = new RAIServer();
         server.listen(PORT_NUMBER, function(err){
             
-            test.expect(2);
+            test.expect(8);
+            
+            server.on("connection", function(socket){
+                
+                socket.on("command", function(command, payload){
+                    if(command == "STATUS"){
+                        test.ok(1, "Status received");
+                    }else if(command=="DATA"){
+                        test.ok(1, "data command received");
+                        socket.startDataMode();
+                    }else if(command=="END"){
+                        test.ok(1, "all received");
+                    }else{
+                        test.ok(0, "Unexpected command: "+command);
+                    }
+                });
+                
+                socket.on("data", function(chunk){
+                    test.equal(chunk.toString(), "TE\r\nST");
+                });
+                
+                socket.on("ready", function(){
+                    test.ok(1, "Data mode ended");
+                });
+                
+                socket.on("end", function(){
+                    test.ok(1, "All ready");
+                    server.end(function(){
+                        test.done();
+                    });
+                });
+                
+                socket.on("error", function(err){
+                    test.isError(err);
+                });
+            });
+            
+            var client = netlib.connect(PORT_NUMBER, function(){
+                client.write("STATUS\r\nSTATUS\r\nSTATUS\r\nDATA\r\nTE\r\nST\r\n.\r\nEND\r\n");
+                client.end();
+            });
+
+        });
+    }
+}
+
+exports["Timeout tests"] = {
+    "Timeout": function(test){
+        var server = new RAIServer({timeout: 300});
+        test.expect(3);
+        server.listen(PORT_NUMBER, function(err){
+            
+            server.on("connection", function(socket){
+                test.ok(socket, "Client connected");
+                
+                socket.on("timeout", function(){
+                    test.ok(1, "Connection closed");
+                    
+                    server.end(function(){
+                        test.done();
+                    });
+                });
+                
+                socket.on("error", function(err){
+                    test.isError(err);
+                });
+            });
+            
+            var client = netlib.connect(PORT_NUMBER, function(){
+                test.ok(1, "Connected to server");
+            });
+
+        });
+    },
+    "Timeout with TLS":  function(test){
+        var server = new RAIServer({timeout: 300});
+        server.listen(PORT_NUMBER, function(err){
+            
+            test.expect(3);
             
             server.on("connection", function(socket){
                 
@@ -222,10 +442,15 @@ exports["General tests"] = {
                     socket.send("TEST");
                 });
                 
-                socket.on("end", function(){
+                socket.on("timeout", function(){
+                    test.ok(1, "Timeout occurred");
                     server.end(function(){
                         test.done();
                     });
+                });
+                
+                socket.on("error", function(err){
+                    test.isError(err);
                 });
             });
             
@@ -238,13 +463,10 @@ exports["General tests"] = {
                 pair.fd = client.fd;
                 
                 pair.on("secure", function(){
-                    pair.cleartext.on("data", function(chunk){
-                        test.equal(chunk.toString(), "TEST\r\n");
-                        pair.cleartext.end();
-                    });
+                    test.ok(1, "secure connection");
                 });
             });
 
         });
     } 
-}
+};
